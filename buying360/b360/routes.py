15469bcfd0441bd2c409c18f1036982afc77d44c
I527231 <elwin.thomas@sap.com>
2024-03-19 10:27:22 +0530

    19 march

--- buying360/b360/routes.py
+++ buying360/b360/routes.py
@@ -11,6 +11,26 @@ import joblib
 from surprise import SVD, Reader, Dataset
 from surprise.model_selection import cross_validate
 
+
+####################   Reused Functions ####################
+
+# Function that takes in productId and useId as input and outputs up to 5 most similar products.
+def hybrid_recommendations(userId, productId, matrix, svd):
+    
+    # Get the Id of the top five products that are correlated with the ProductId chosen by the user.
+    top_five=matrix.corrwith(matrix[productId]).sort_values(ascending=False).head(5)
+    
+    # Predict the ratings the user might give to these top 5 most correlated products.
+    est_rating=[]
+    for x in list(top_five.index):
+        if str(top_five[x])!='nan':
+            est_rating.append(svd.predict(userId, iid=x, r_ui=None).est)
+           
+    return pd.DataFrame({'productId':list(top_five.index)[:len(est_rating)], 'estimated_rating':est_rating}).sort_values(by='estimated_rating', ascending=False).reset_index(drop=True)
+
+
+############## add to Cart func ##################
+
 @app.route('/')
 @app.route('/home',methods= ['GET','POST'])
 def home_page():
@@ -77,6 +97,44 @@ def process_search():
     print(search_query)
     return redirect(url_for('catalog_page', search_query = search_query))
 
+def addToCart():
+    if request.method == 'POST':
+        #Add to cart logic
+        add_to_cart_item = request.form.get('add_to_cart_item')
+        add_to_cart_item_object = Products.query.filter_by(id=add_to_cart_item).first()
+        if add_to_cart_item_object:
+            line_item = CartLineItem.query.filter_by(
+                product_id=add_to_cart_item_object.id,
+                user_id = current_user.user_id).first()
+            #### update quantity and price if it exists.
+            if line_item:
+                line_item.quantity = line_item.quantity + 1
+                line_item.total_price = add_to_cart_item_object.price * line_item.quantity
+            ### set quantity and total price for first item insertion    
+            else:
+                quantity = 1
+                total_price = add_to_cart_item_object.price
+
+                lineItemToInsert = CartLineItem(
+                product_id = add_to_cart_item_object.id,
+                user_id = current_user.user_id,
+                product_name = add_to_cart_item_object.product_name,                
+                product_description = add_to_cart_item_object.product_description,
+                product_img= "abc",
+                price = add_to_cart_item_object.price,
+                quantity = quantity,
+                total_price = total_price
+                )
+                db.session.add(lineItemToInsert)
+                db.session.commit()
+                        
+                        
+            # current_user.user_cart.contains(add)
+            add_to_cart_item_object.add_to_cart_for_user(current_user)
+
+                    
+            flash(f"Item added to cart.", category='success')    
+    return
 
 @app.route('/catalog', methods = ['GET','POST'])
 @login_required
@@ -88,68 +146,80 @@ def catalog_page():
     add_to_cart_form = AddToCart()
     lineItemToInsert = CartLineItem()
     if add_to_cart_form.validate_on_submit():
-        if request.method == 'POST':
-            #Add to cart logic
-            add_to_cart_item = request.form.get('add_to_cart_item')
-            add_to_cart_item_object = Products.query.filter_by(id=add_to_cart_item).first()
-            if add_to_cart_item_object:
-                line_item = CartLineItem.query.filter_by(
-                    product_id=add_to_cart_item_object.id,
-                    user_id = current_user.user_id).first()
-                #### update quantity and price if it exists.
-                if line_item:
-                    line_item.quantity = line_item.quantity + 1
-                    line_item.total_price = add_to_cart_item_object.price * line_item.quantity
-                ### set quantity and total price for first item insertion    
-                else:
-                    quantity = 1
-                    total_price = add_to_cart_item_object.price
+        addToCart()
+        # if request.method == 'POST':
+        #     #Add to cart logic
+        #     add_to_cart_item = request.form.get('add_to_cart_item')
+        #     add_to_cart_item_object = Products.query.filter_by(id=add_to_cart_item).first()
+        #     if add_to_cart_item_object:
+        #         line_item = CartLineItem.query.filter_by(
+        #             product_id=add_to_cart_item_object.id,
+        #             user_id = current_user.user_id).first()
+        #         #### update quantity and price if it exists.
+        #         if line_item:
+        #             line_item.quantity = line_item.quantity + 1
+        #             line_item.total_price = add_to_cart_item_object.price * line_item.quantity
+        #         ### set quantity and total price for first item insertion    
+        #         else:
+        #             quantity = 1
+        #             total_price = add_to_cart_item_object.price
 
-                    lineItemToInsert = CartLineItem(
-                    product_id = add_to_cart_item_object.id,
-                    user_id = current_user.user_id,
-                    product_name = add_to_cart_item_object.product_name,                
-                    product_description = add_to_cart_item_object.product_description,
-                    product_img= "abc",
-                    price = add_to_cart_item_object.price,
-                    quantity = quantity,
-                    total_price = total_price
-                    )
-                    db.session.add(lineItemToInsert)
-                    db.session.commit()
+        #             lineItemToInsert = CartLineItem(
+        #             product_id = add_to_cart_item_object.id,
+        #             user_id = current_user.user_id,
+        #             product_name = add_to_cart_item_object.product_name,                
+        #             product_description = add_to_cart_item_object.product_description,
+        #             product_img= "abc",
+        #             price = add_to_cart_item_object.price,
+        #             quantity = quantity,
+        #             total_price = total_price
+        #             )
+        #             db.session.add(lineItemToInsert)
+        #             db.session.commit()
+                        
+                        
+        #         # current_user.user_cart.contains(add)
+        #         add_to_cart_item_object.add_to_cart_for_user(current_user)
+
                     
-                    
-                # current_user.user_cart.contains(add)
-                add_to_cart_item_object.add_to_cart_for_user(current_user)
-
-                
-                flash(f"Item added to cart.", category='success')
-            
-
-
-
+        #         flash(f"Item added to cart.", category='success')        
+        
     return render_template('catalog.html', add_to_cart_form = add_to_cart_form, searched_products = searched_products, search_query = search_query)
 
 
-# Function that takes in productId and useId as input and outputs up to 5 most similar products.
-def hybrid_recommendations(userId, productId, matrix, svd):
-    
-    # Get the Id of the top five products that are correlated with the ProductId chosen by the user.
-    top_five=matrix.corrwith(matrix[productId]).sort_values(ascending=False).head(5)
-    
-    # Predict the ratings the user might give to these top 5 most correlated products.
-    est_rating=[]
-    for x in list(top_five.index):
-        if str(top_five[x])!='nan':
-            est_rating.append(svd.predict(userId, iid=x, r_ui=None).est)
-           
-    return pd.DataFrame({'productId':list(top_five.index)[:len(est_rating)], 'estimated_rating':est_rating}).sort_values(by='estimated_rating', ascending=False).reset_index(drop=True)
 
 @app.route('/cart', methods = ['GET','POST'])
 @login_required
 def cart_page():
     line_items = CartLineItem.query.filter_by(user_id = current_user.user_id).all()
     submit_request_form = SubmitPRForm()
+    add_to_cart_form = AddToCart()
+    ###recommendations logic
+    recommendations_list = []       ##### this list will contain the product ids of recommended products based on current cart
+    recommended_products_from_list = []
+
+    for line_item in line_items:
+        ###recommendations
+        column_names=['id', 'brand', 'name', 'reviews.rating', 'reviews.username']
+        df=pd.read_csv('/Users/I527231/Documents/Dissertation/buying360/b360/data/Reviews.csv',names=column_names)
+        matrix=pd.pivot_table(data=df, values='reviews.rating', index='reviews.username',columns='id')
+        svd = joblib.load('/Users/I527231/Documents/Dissertation/buying360/b360/ml_models/svd_model.pkl')
+        df = hybrid_recommendations(current_user.username, line_item.product_id, matrix, svd)
+        recommendations_list.append(df['productId'].tolist())
+        print("recommendations list:")
+        print(recommendations_list)
+
+    #####get each product from product id in recommendations
+    for sublist in recommendations_list:
+        for product_id in sublist:
+            recommended_products_from_list.append(Products.query.filter_by(id=product_id).first())
+
+
+
+    ###add to cart of recommended products
+    
+                  
+
     if submit_request_form.validate_on_submit():
         generated_pr_id = random.randint(1000, 9999)
         total_pr_price = 0
@@ -173,22 +243,14 @@ def cart_page():
             ##delete from cart_line_items table
             db.session.delete(line_item)
             db.session.commit()
-
-            
+            print("submitted")
 
         flash(f"Purchase Requisition Request submitted successfully.", category='success')
         return redirect(url_for('pr_page',generated_pr_id = generated_pr_id))
 
-    ###recommendations
-    column_names=['id', 'brand', 'name', 'reviews.rating', 'reviews.username']
-    df=pd.read_csv('/Users/I527231/Documents/Dissertation/buying360/b360/data/Reviews.csv',names=column_names)
-    matrix=pd.pivot_table(data=df, values='reviews.rating', index='reviews.username',columns='id')
-    svd = joblib.load('/Users/I527231/Documents/Dissertation/buying360/b360/ml_models/svd_model.pkl')
-    hybrid_recommendations('Adam Czap', 'AVpgGPyq1cnluZ0-wbTJ', matrix, svd)
+    return render_template('cart.html', line_items = line_items, submit_request_form = submit_request_form, recommended_products = recommended_products_from_list, add_to_cart_form = add_to_cart_form)
 
 
-
-    return render_template('cart.html', line_items = line_items, submit_request_form = submit_request_form)
 
 @app.route('/PR', methods = ['GET','POST'])
 @login_required
